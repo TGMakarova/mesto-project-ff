061a24d77415cd1104fe540ff1f92c46086f4e81
Makarova Tatiana <masterfriend@yandex.ru>
2023-12-13 14:45:27 +0300

    commit_1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+Автор: [Макарова Т.Г.](https://github.com/TGMakarova/mesto-project-ff "GitHub Репозиторий")
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+function addCard(name, link, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = link;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  const listItem = event.target.closest(".places__item");
+  listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) =>
+  placesList.append(addCard(item.name, item.link, deleteCard))
+);
